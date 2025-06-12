ee94abbedc840b6125a8896a1a1cad82f9ef2c1d
Sarath <sarathmenon@mailbox.org>
2025-06-12 10:38:24 +0200

    further refine workflows

--- workflows/freenergy.py
+++ workflows/freenergy.py
@@ -21,3 +21,56 @@ def calculate_free_energy(
     )
     job.run()
     return job.output.temperature, job.output.energy_free
+
+
+def calculate_quasiharmonic_free_energy(
+    structure, potential, temperature, temperature_steps=2, cores=4
+):
+    pr = Project("FreeEnergyQH1")
+    pr.remove_jobs(recursive=True)
+
+    refjob = pr.create.job.Lammps("reflmp")
+    refjob.structure = structure
+    refjob.potential = potential
+
+    phono = pr.create.job.PhonopyJob("phono")
+    phono.ref_job = refjob
+
+    quasi = pr.create.job.QuasiHarmonicJob("quasi")
+    quasi.ref_job = phono
+    quasi.server.cores = cores
+
+    quasi.input["temperature_end"] = temperature
+    quasi.input["temperature_steps"] = temperature_steps
+    quasi.input["axes"] = ["x", "y", "z"]
+    quasi.input["vol_range"] = 0.01
+    quasi.input["strains"] = None
+
+    quasi.run()
+
+    ejob = pr.create.job.Lammps("lmp1", delete_existing_job=True)
+    ejob.structure = structure
+    ejob.potential = potential
+
+    ejob.calc_minimize(pressure=0)
+    ejob.run()
+
+    int_energy = ejob.output.energy_pot[-1] / len(structure)
+    temps = quasi["output"]["temperatures"][0]
+    fes = quasi["output"]["free_energy"][0] + int_energy
+
+    return temps, fes
+
+
+def calculate_internal_energy(structure, potential):
+    pr = Project("InternalEnergy")
+    pr.remove_jobs(recursive=True)
+
+    job = pr.create.job.Lammps("lmp1", delete_existing_job=True)
+    job.structure = structure
+    job.potential = potential
+
+    job.calc_minimize(pressure=0)
+    job.run()
+
+    return job.output.energy_pot[-1] / len(structure)
--- workflows/potentials.py
+++ workflows/potentials.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import os
 
-potential = pd.DataFrame(
+filename_Fe_Ack = os.path.join(os.path.dirname(__file__), "potentials/Fe_Ack.eam")
+
+EAM_Ackland_Fe = pd.DataFrame(
     {
         "Config": [["pair_style eam/alloy\n", "pair_coeff * * Fe_Ack.eam Fe\n"]],
-        "Filename": [["/u/smenon/Fe-workflows/Fe_Ack.eam"]],
+        "Filename": [[filename_Fe_Ack]],
         "Model": ["EAM"],
         "Name": ["EAMAckNin"],
         "Species": [["Fe"]],
